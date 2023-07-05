2e19dcfaf7546c6b9c8dd71eb3e0c09d3ae55ec2
BibekM <bibekmbkb@gmail.com>
2023-07-05 14:44:16 -0700

    fixed save button issues

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FIND MY PET BESTFRIEND APP
 
 ## DESCRIPTION
-  This website helps the user to find and adopt pets from their nearest location.
+This website helps the user to find and adopt pets from their nearest location.
 
 ## USAGE 
 when the user enters the website , the user is presented with the homepage that lhas navigation buttons for pet search .
--- public/js/delete.js
+++ public/js/delete.js
@@ -2,16 +2,18 @@ const delButtonHandler = async (event) => {
     event.preventDefault();
 
     if (event.target.matches(".unsaveBtn")) {
-    let btnElId = event.target.id;
-    console.log("Trying to delete  ", btnElId)
-            const response = await fetch(`/api/pets/${{btnElId}}`, {
-        method: 'DELETE',
-    });
+        let btnElId = event.target.id;
+        console.log("Trying to delete  ", btnElId)
 
-        if (response.ok) {
+        const response = await fetch(`/api/pets/${btnElId}`, {//TODO: delete pet does not work
+            method: 'DELETE',
+        });
+
+        if(response.ok) {
+            console.log("Pet Deleted");
             document.location.replace('/pets');
         } else {
-            alert('Failed to delete project');
+            alert('Failed to unsave pet');
         }
     }
 };
